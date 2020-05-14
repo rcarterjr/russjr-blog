a78fabd216e6fdbcd7ff9ca57ff2873850b59225
Russell Carter <russell.c12@gmail.com>
2020-05-14 19:46:44 -0400

    move icons to src

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `public/icons/favicon.ico`, // This path is relative to the root of the site.
+                icon: `src/icons/favicon.ico`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
